269fba1c54f97ae313d347e474dd4e56786b214f
shinichirot <78186249+shinichirot@users.noreply.github.com>
2021-11-21 19:13:00 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+var params = (new URL(document.location)).searchParams;
+var key = params.get('key');
+
+$(function () {
+    // 騾∽ｿ｡
+    $('form').submit(function () {
+
+        sendText(`情報登録ありがとうございました`);
+        
+        var name = $('textarea[name="name"]').val();
+        var adress = $('textarea[name="adress"]').val();
+        var tel = $('textarea[name="tel"]').val();
+        var age = $('select[name="age"]').val();
+        var kanshin = $("input:radio[name='kanshin']:checked").val();
+        var how = $("input:radio[name='how']:checked").val();
+        var where = $("input:radio[name='where']:checked").val();
+        var date = $('input[name="date"]').val();
+       
+        var msg = `◆回答ありがとうございます\n名前：${name}\n郵便番号：${adress}\n電話番号：${tel}\n年代：${age}\n関心：${kanshin}\n連絡方法：${how}\n面談場所：${where}\n面談希望日：${date}`;
+        sendText(msg);
+
+        return false;
+    });
+});
